378466c9a839061d5a8e4653667937bb354f53e4
Reinaldo Barreto da Silva <92860945+reinaldobarreto31@users.noreply.github.com>
2026-07-30 06:12:48 -0300

    feat(ui): add XML layouts, Fragments, Navigation graph, and TaskAdapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt.android)
+    alias(libs.plugins.navigation.safe.args)
 }
 
 android {
